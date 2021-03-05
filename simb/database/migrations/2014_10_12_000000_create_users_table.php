a639d69fbb65c1e88ecaad53d8af2d57fab24908
Borix102 <borissandeu0@gmail.com>
2021-03-05 16:04:02 +0100

    Fix

--- simb/database/migrations/2014_10_12_000000_create_users_table.php
+++ simb/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->foreign('parent_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
